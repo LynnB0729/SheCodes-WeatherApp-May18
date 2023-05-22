9c58ea6b584874046d6ba770a01a8dc8a6f9e0e7
Lynn <simplylynnhandmade@gmail.com>
2023-05-21 23:37:15 -0400

    saving

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 //to display current, date and time to user 
 window.addEventListener('load', function() {
   // Get the current date and time
@@ -6,7 +7,7 @@ window.addEventListener('load', function() {
   var currentTime = currentDate.toLocaleTimeString();
 
   // Update the HTML elements with the current date, location, and time
-  document.getElementById('date').textContent = 'Current Date: ' + currentDate.toDateString();
+  document.getElementById('date').textContent = currentDate.toDateString();
   document.getElementById('location').textContent = 'Current Location: ' + currentLocation;
   document.getElementById('time').textContent = 'Current Time: ' + currentTime;
 });
@@ -33,12 +34,13 @@ $(document).ready(function () {
                     var tempC = ((tempF - 32) * 5 / 9).toFixed(2);
                     var weatherDescription = data.weather[0].description;
 
-                    $('.wind').html('wind speed: ' + wind);
-                    $('.humidity').html('humidity: ' + humidity);
-                    $('.cloudiness').html('cloudiness: ' + clouds);
+                    $('.wind').html('wind speed: ' + wind + ' mph');
+                    $('.humidity').html('humidity level: ' + humidity + '%');
+                    $('.cloudiness').html('cloudiness: ' + clouds + '%');
                     $('.farenheit-temp').html(tempF + 'ºF');
                     $('.celsius-temp').html(tempC + 'ºC');
                     $('.weather-description').html(weatherDescription);
+                    $('#city-name').html(city);
 
                     var options = { timeZone: data.timezone, hour: 'numeric', minute: 'numeric', hour12: false, year: 'numeric', month: 'numeric', day: 'numeric' };
                     var date = new Date().toLocaleString([], options);
@@ -50,3 +52,5 @@ $(document).ready(function () {
         }
     });
 });
+
+
