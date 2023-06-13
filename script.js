038758ca975133f6ce5b9743446ce97337d0dd4e
Lynn <simplylynnhandmade@gmail.com>
2023-06-13 18:45:44 -0400

    saving changes

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 //for the OpenWeather API Functionality
 var cityInput = document.getElementById('city');
 var submitButton = document.getElementById('submit');
-
-
 var apiKey = 'cff2aa1e3c91c0248805f01bed83e69c';
 
 function getWeatherInfo() {
     var city = cityInput.value;
-
     if (city != '') {
         axios.get('https://api.openweathermap.org/data/2.5/weather', {
             params: {
@@ -75,48 +72,58 @@ function getForecast(lat, lon) {
     console.log('Latitude: ' + lat);
     console.log('Longitude: ' + lon);
 
-    axios.get('https://api.openweathermap.org/data/2.5/weather', {
+    axios.get(`https://api.openweathermap.org/data/2.5/onecall`, {
             params: {
                 appid: apiKey,
                 lat: lat,
                 lon: lon,
-                units: 'imperial'
+                units: 'imperial',
+                exclude: 'current,minutely,hourly,alerts'
             }
-            })
+        })
     .then(function(response) { 
+        console.log("it worked")
         // Handle the response here, e.g., update the forecast
-        console.log(response.data);
+        displayForecast(response.data);
+       
     })
     .catch(function(error) {
         console.error(error);
     });
+    console.log("hello");
 }
 
-
-function displayForecast() {
-    let forecastElement = document.querySelector("#forecast");
+function displayForecast(data) {
+    console.log("it worked ");
+    var forecastElement = document.querySelector("#forecast");
     
-    let forecastHTML = `<div class="row">`;
-    let days = ["Thursday", "Friday", "Saturday", "Sunday"];
-    days.forEach(function(day){
+    var forecastHTML = `<div class="row">`;
+        for(let i = 0; i < 5; i++) {
+        var date = new Date(data.daily[i].dt * 1000); // Convert from Unix timestamp to JavaScript date
+        var day = date.toLocaleString('en-US', { weekday: 'long' }); // Get the day name
+        var highTemp = data.daily[i].temp.max.toFixed(2);
+        var lowTemp = data.daily[i].temp.min.toFixed(2);
+        var icon = data.daily[i].weather[0].icon;
+        var iconUrl = `http://openweathermap.org/img/wn/${icon}.png`;
+        //this is what it looked like hard coded --> 
+        //let days = ["Thursday", "Friday", "Saturday", "Sunday"];
+        //days.forEach(function(day){
 
          forecastHTML = forecastHTML + `
     
     <div class="col">
                 ${day}
-                <img src="Weather (16) PNG.png" alt="" width="36">
+                <img src="${iconUrl}" alt="" width="36">
                 <div class="weather-forecast-temperatures">
-                    <span class="weather-forecast-temperature-max">18º</span>
-                    <span class="weather-forecast-temperature-min">12º</span>
+                    <span class="weather-forecast-temperature-max">${highTemp}º</span>
+                    <span class="weather-forecast-temperature-min">${lowTemp}º</span>
                 </div>
             </div>
-    
     </div>
     `; 
-    })
+    }
     
     forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
-} 
+};
 
-displayForecast();
